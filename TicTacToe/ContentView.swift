643331cc5fb52c4014930f82c71ef83f6cc87820
Julie Go <julie.gosselin84@yahoo.fr>
2022-07-11 11:37:25 +0200

    first commit "button commencer" et un bouton test de jeu

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let IpsumLorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ex turpis, bibendum sed ante non, pretium mollis eros. Aenean tristique at nisl at maximus.  "
+    
+        VStack {
+            NavigationView {
+                VStack {
+                    Text(IpsumLorem)
+                    Spacer()
+                    NavigationLink(destination: BoardGameView()) {
+                        Text("COMMENCER")
+                    }
+                    Spacer()
+                    
+            }
+        }
+      }
     }
 }
 
@@ -19,3 +33,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
